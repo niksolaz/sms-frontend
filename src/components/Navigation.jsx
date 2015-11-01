acce84b48e4c6ec8e7d265b3f75f41399c10d85f
Nik Solaz <solazzo.nicola@gmail.com>
2015-11-01 14:25:35 +0000

    img slice

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -13,9 +13,7 @@ export default class Navigation extends Component {
                 <span className="icon-bar"></span>
                 <span className="icon-bar"></span>
               </button>
-              <a className="navbar-brand" href="#">
-                <img alt="Brand" src="img/slice.png">
-              </a>
+              <a className="navbar-brand" href="#"><img alt="Brand" src="img/slice.png"></a>
             </div>
         
             <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
