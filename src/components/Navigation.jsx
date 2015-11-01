b3a8756cc39081e6a9c468e64d44fcd63a405118
Nik Solaz <solazzo.nicola@gmail.com>
2015-11-01 14:34:37 +0000

    insert img in src dir

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -13,9 +13,8 @@ export default class Navigation extends Component {
                 <span className="icon-bar"></span>
                 <span className="icon-bar"></span>
               </button>
-              <a className="navbar-brand" href="#"><img alt="Brand" src="../Slice.png"></a>
+              <a className="navbar-brand" href="#"><img alt="Brand" src="img/Slice.png"></a>
             </div>
-        
             <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
               <ul className="nav navbar-nav">
                 <li className="active"><a href="#">LinkOne <span className="sr-only">(current)</span></a></li>
