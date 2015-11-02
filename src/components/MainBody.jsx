183ebe26769087de1f30133e77663c7994adab18
Nik Solaz <solazzo.nicola@gmail.com>
2015-11-02 15:45:26 +0000

    clean button

--- src/components/MainBody.jsx
+++ src/components/MainBody.jsx
@@ -7,10 +7,6 @@ export default class MainBody extends Component {
         <div class="jumbotron">
           <h1>Welcome on Smsl!</h1>
           <p>This web application allows you to send a single message simultaneously to the major social networks.</p>
-          <textarea className="textarea"></textarea>
-          <button type="button" class="btn btn-primary" data-toggle="button" aria-pressed="false" autocomplete="off">
-            Send Message
-          </button>
         </div>
       </div>
     );
