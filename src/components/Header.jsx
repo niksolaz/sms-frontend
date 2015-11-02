87e009f8162f2c3ad3bd5670037495eb0575fc6b
Nik Solaz <solazzo.nicola@gmail.com>
2015-11-02 14:44:41 +0000

    change nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,41 +14,6 @@ export default class Header extends Component {
               Smsl <small> instant message </small>
             </h1>
             <h3> Send Messages Simultaneously to Twitter and Facebook </h3>
-            <div id="carousel-example-generic" className="carousel slide" data-ride="carousel">
-              
-              <ol className="carousel-indicators">
-                <li data-target="#carousel-example-generic" data-slide-to="0" className="active"></li>
-                <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-                <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-              </ol>
-            
-              
-              <div className="carousel-inner" role="listbox">
-                <div className="item">
-                  <img src="http://i.telegraph.co.uk/multimedia/archive/03375/twitter2_3375790k.jpg"/>
-                  <div className="carousel-caption">
-                   for Twitter
-                  </div>
-                </div>
-                <div className="item">
-                  <img src="http://st.ilfattoquotidiano.it/wp-content/uploads/2015/05/facebook-6751.jpg"/>
-                  <div className="carousel-caption">
-                    for Facebook
-                  </div>
-                </div>
-                ...for other
-              </div>
-            
-              
-              <a className="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-                <span className="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-                <span className="sr-only">Previous</span>
-              </a>
-              <a className="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-                <span className="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-                <span className="sr-only">Next</span>
-              </a>
-            </div>
           </div>
         </div>
       </div>
