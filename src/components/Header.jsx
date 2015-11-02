c25b532258cf1d0311109e7ad8ea318865e70925
Nik Solaz <solazzo.nicola@gmail.com>
2015-11-02 14:36:22 +0000

    change className

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,13 +18,13 @@ export default class Header extends Component {
               
               <ol className="carousel-indicators">
                 <li data-target="#carousel-example-generic" data-slide-to="0" className="active"></li>
-                <li data-target="#carousel-example-generic" data-slide-to="1" className="active"></li>
-                <li data-target="#carousel-example-generic" data-slide-to="2" className="active"></li>
+                <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                <li data-target="#carousel-example-generic" data-slide-to="2"></li>
               </ol>
             
               
               <div className="carousel-inner" role="listbox">
-                <div className="item active">
+                <div className="item">
                   <img src="http://i.telegraph.co.uk/multimedia/archive/03375/twitter2_3375790k.jpg"/>
                   <div className="carousel-caption">
                    for Twitter
