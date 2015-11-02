f9a5c5d23a5130fbf29eb0b23c999f8668312040
Nik Solaz <solazzo.nicola@gmail.com>
2015-11-02 15:20:08 +0000

    new header

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -44,43 +44,6 @@ export default class Navigation extends Component {
               </form>
             </div>
           </div>
-          
-          
-          
-          
-          <div id="carousel-example-generic" className="carousel slide" data-ride="carousel">
-              <ol className="carousel-indicators">
-                <li data-target="#carousel-example-generic" data-slide-to="0" clasName="active"></li>
-                <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-                <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-              </ol>
-
-              <div className="carousel-inner" role="listbox">
-                <div className="item active">
-                  <img src="http://i.telegraph.co.uk/multimedia/archive/03375/twitter2_3375790k.jpg"/>
-                  <div className="carousel-caption">
-                   for Twitter
-                  </div>
-                </div>
-                <div className="item">
-                  <img src="http://st.ilfattoquotidiano.it/wp-content/uploads/2015/05/facebook-6751.jpg"/>
-                  <div className="carousel-caption">
-                    for Facebook
-                  </div>
-                </div>
-              </div>
-            
-              <a className="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-                <span className="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-                <span className="sr-only">Previous</span>
-              </a>
-              <a className="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-                <span className="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-                <span className="sr-only">Next</span>
-              </a>
-            </div>
-            
-            
         </nav>
       </div>
     );
