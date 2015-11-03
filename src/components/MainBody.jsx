16ca2a5a25c99c91a3c58ff2260294bf38e33933
Nik Solaz <solazzo.nicola@gmail.com>
2015-11-03 18:19:50 +0000

    mainbody bar

--- src/components/MainBody.jsx
+++ src/components/MainBody.jsx
@@ -4,9 +4,12 @@ export default class MainBody extends Component {
   render() {
     return (
       <div>
-        <div className="jumbotron">
+        <div className="jumbotron-inverse">
           <h1>Welcome on Smsl!</h1>
           <p>This web application allows you to send a single message simultaneously to the major social networks.</p>
+          <section>
+          
+          </section>
         </div>
       </div>
     );
