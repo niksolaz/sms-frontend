3df2e657e961c9a8e33070be5432da09466c0241
Nik Solaz <solazzo.nicola@gmail.com>
2015-11-03 18:01:47 +0000

    navigation bar

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -23,18 +23,6 @@ export default class Navigation extends Component {
                 <li><a href="#">LinkTwo</a></li>
                 <li><a href="#">LinkThree</a></li>
                 <li><a href="#">LinkFour</a></li>
-                <li className="dropdown">
-                  <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span className="caret"></span></a>
-                  <ul className="dropdown-menu">
-                    <li><a href="#">Action</a></li>
-                    <li><a href="#">Another action</a></li>
-                    <li><a href="#">Something else here</a></li>
-                    <li role="separator" className="divider"></li>
-                    <li><a href="#">Separated link</a></li>
-                    <li role="separator" className="divider"></li>
-                    <li><a href="#">One more separated link</a></li>
-                  </ul>
-                </li>
               </ul>
               <form className="navbar-form navbar-left" role="search">
                 <div className="form-group">
