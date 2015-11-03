16780dba6d6109049888dbe896ae6ae8d985d78d
Nik Solaz <solazzo.nicola@gmail.com>
2015-11-03 19:10:48 +0000

    carousel

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,14 +17,14 @@ export default class Header extends Component {
           </div>
           <div id="myCarousel" className="carousel slide">
             <ol className="carousel-indicators">
-              <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+              <li data-target="#myCarousel" data-slide-to="0" className="active"></li>
               <li data-target="#myCarousel" data-slide-to="1"></li>
               <li data-target="#myCarousel" data-slide-to="2"></li>
             </ol>
             <div className="carousel-inner">
-              <div className="active item">…</div>
-              <div className="item">…</div>
-              <div className="item">…</div>
+              <div className="active item"><img src="http://4.bp.blogspot.com/-ucEWV55BuAQ/VLA8kQTp61I/AAAAAAAAahw/4Sgbe1q8RhM/s1600/ricevere%2Bsms.png" alt="...">…</div>
+              <div className="item"><img src="http://www.dirittodicritica.com/wp-content/uploads/2015/09/facebook1.jpg" alt="...">…</div>
+              <div className="item"><img src="http://www.finzionimagazine.it/wp-content/uploads/2015/07/twitter-ipo_.jpg" alt="...">…</div>
             </div>
             <a className="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
             <a className="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
