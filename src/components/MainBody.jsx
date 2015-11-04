3f71f61d19b97312d1806c5e74bfc62b13c210b3
Nik Solaz <solazzo.nicola@gmail.com>
2015-11-04 14:52:55 +0000

    modified css

--- src/components/MainBody.jsx
+++ src/components/MainBody.jsx
@@ -4,14 +4,12 @@ export default class MainBody extends Component {
   render() {
     return (
       <div>
-        <div className="pagination pagination-centered">
-          <div className="jumbotron">
-            <h1>Welcome on Smsl!</h1>
-            <p>This web application allows you to send a single message simultaneously to the major social networks.</p>
-            <section>
-            
-            </section>
-          </div>
+        <div className="jumbotron">
+          <h1 className="text-h1">Welcome on Smsl!</h1>
+          <p className="text-h1">This web application allows you to send a single message simultaneously to the major social networks.</p>
+          <section>
+          
+          </section>
         </div>
       </div>
     );
