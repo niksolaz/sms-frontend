52c6548274df84dd5b9ed98cbd4560c0bc8bd3fd
Nik Solaz <solazzo.nicola@gmail.com>
2015-11-06 17:26:22 +0000

    navbar in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,11 @@ export default class App extends Component {
   render() {
     return (
       <div className="container">
-          <Navigation />
+        
           <Header />
           <MainBody />
           <Footer />
-        
+        </div>
       </div>
     );
   }
