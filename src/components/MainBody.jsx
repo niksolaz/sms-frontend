e4f732a8652c9d44822fe36bb5a4f31b09a932c7
Nik Solaz <solazzo.nicola@gmail.com>
2015-11-07 15:00:46 +0000

    button type classic

--- src/components/MainBody.jsx
+++ src/components/MainBody.jsx
@@ -8,7 +8,9 @@ export default class MainBody extends Component {
           <h1 className="text-center">Welcome on Smsl!</h1>
           <p className="text-center">This web application allows you to send a single message simultaneously to the major social networks.</p>
           <section>
-            <button type="button">Click Me!</button>
+            <button type="button" class="btn btn-primary">Facebook</button>
+            <button type="button" class="btn btn-info">Twitter</button>
+            <button type="button" class="btn btn-danger">Google+</button>
           </section>
         </div>
       </div>
