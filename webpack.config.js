11526474b987613725feb2076fe71f8a8fc83f85
Nik Solaz <solazzo.nicola@gmail.com>
2015-11-08 16:09:49 +0000

    add test and loaders in webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -18,8 +18,8 @@ module.exports = {
   ],
   module: {
     loaders: [{
-      test: /\.jsx$/,
-      loaders: ['react-hot', 'babel'],
+      test: [/\.jsx$/,/\.css$/],
+      loaders: ['react-hot', 'babel','bootstrap'],
       include: path.join(__dirname, 'src'),
       exclude: path.join(__dirname, 'node_modules'),
     }]
