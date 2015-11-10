82f0a6d9d345b457aedc0216c44571974efdf6aa
Nik Solaz <solazzo.nicola@gmail.com>
2015-11-10 17:00:16 +0000

    added other components in navigation: Facebook and twitter page, login page

---
+++ src/components/FacebookPage.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class FacebookPage extends Component {
+    render (){
+        return(
+            <div>
+            
+            </div>
+        );
+    }
+}
---
+++ src/components/TwitterPage.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class TwitterPage extends Component {
+    render (){
+        return(
+            <div>
+            
+            </div>
+        );
+    }
+}
