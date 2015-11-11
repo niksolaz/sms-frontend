7383b5aacb517b024ea639930314d8de11be97b7
Nik Solaz <solazzo.nicola@gmail.com>
2015-11-11 15:54:02 +0000

    up to date components login page

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -4,11 +4,13 @@ export default class LoginPage extends Component {
     render (){
         return(
             <div>
-                <form>
-                <input type="text" name="Email">Email</input><br/>
-                <input type="text" name="Password">Password</input><br/>
-                <input type="submit" value="Submit">GO!</input>
-                </form>
+                <div className='LoginContainer'>
+                    <form>
+                    <input type="text" name="Email">Email</input><br/>
+                    <input type="text" name="Password">Password</input><br/>
+                    <input type="submit" value="Submit">GO!</input>
+                    </form>
+                </div>
             </div>
         );
     }
