80caf3bbec3bbdfed3c387a34d9b49373ebd47b7
Nik Solaz <solazzo.nicola@gmail.com>
2015-11-11 16:05:22 +0000

    up to date components Navigation bar with links

--- src/components/MainBody.jsx
+++ src/components/MainBody.jsx
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import HomePage from './HomePage.jsx';
+import FacebookPage from './FacebookPage.jsx';
+import TwitterPage from './TwitterPage.jsx';
+import LoginPage from './LoginPage.jsx';
 
 export default class MainBody extends Component {
   render() {
     return (
       <div>
-        
+        <HomePage />
+        <FacebookPage />
+        <TwitterPage />
+        <LoginPage />
       </div>
     );
   }
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 
+
 export default class Navigation extends Component {
   render() {
     return (
       <div>
-        <HomePage />
-        <FacebookPage />
-        <TwitterPage />
-        <LoginPage />
+        <div className='NavigationContainer'>
+          <a>Home</a>
+          <a>Facebook</a>
+          <a>Twitter</a>
+          <form>
+            Search:
+            <input type="search" name="search"></input>
+          </form>
+          <a>Login</a>
+          <a>Sign Up</a>
+        </div>
       </div>
     );
   }
