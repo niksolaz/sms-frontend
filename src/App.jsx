2edb34652b553f8c8ee1255acf09f10858abd881
Nik Solaz <solazzo.nicola@gmail.com>
2015-11-12 17:45:46 +0000

    in app.jsx ricalled files components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import Header from '../components/modules/Header.jsx';
-import Footer from '../components/modules/Footer.jsx';
-import MainBody from '../components/modules/MainBody.jsx';
+import Header from './components/modules/Header.jsx';
+import Footer from './components/modules/Footer.jsx';
+import MainBody from './components/modules/MainBody.jsx';
 
 export default class App extends Component {
   render() {
