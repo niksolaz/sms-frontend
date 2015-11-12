ae4e523aafb7bd3e5e2342d284f4e3ce1a659345
Nik Solaz <solazzo.nicola@gmail.com>
2015-11-12 17:23:08 +0000

    correct import in app.jsx for new directory modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import Header from './components/Header.jsx';
-import Footer from './components/Footer.jsx';
-import MainBody from './components/MainBody.jsx';
+import Header from './components/modules/Header.jsx';
+import Footer from './components/modules/Footer.jsx';
+import MainBody from './components/modules/MainBody.jsx';
 
 export default class App extends Component {
   render() {
