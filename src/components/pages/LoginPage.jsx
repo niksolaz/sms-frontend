52263a6d90b6815bebf34b7679ac19cef4fa233b
Nik Solaz <solazzo.nicola@gmail.com>
2015-11-13 17:39:50 +0000

    little modified in Login Page

--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -12,7 +12,8 @@ export default class LoginPage extends Component {
                             <input type="text" name="email"></input>
                             <br/>
                             Password:<br/>
-                            <input type="password" name="psw"></input><br/>
+                            <input type="password" name="psw"></input>
+                            Confirm Your Login:<br/>
                             <input>GO!</input>
                         </fieldset>
                     </form>
