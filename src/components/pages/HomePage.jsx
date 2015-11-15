208fa36c22e158b6e4ad05b63b727031dc5a629f
Nik Solaz <solazzo.nicola@gmail.com>
2015-11-15 14:08:18 +0000

    modified HomePage for router

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-export default class HomePage extends Component {
+let HomePage = React.createClass({
     render (){
         return(
             <div>
@@ -13,4 +13,6 @@ export default class HomePage extends Component {
             </div>
         );
     }
-}
+});
+
+export default HomePage;
