d885e58f5822c07e615a420d124e5c095ec9ec10
Nik Solaz <solazzo.nicola@gmail.com>
2015-11-16 15:07:33 +0000

    isolate login components with comments

--- src/components/shared/Navigation.jsx
+++ src/components/shared/Navigation.jsx
@@ -2,18 +2,18 @@ import React from 'react';
 import Router from 'react-router';  
 import { DefaultRoute, Link, Route, RouteHandler } from 'react-router';
 
-import HomePage from '../components/pages/HomePage.js';
+{/*import HomePage from '../components/pages/HomePage.js';
 import FacebookPage from '../components/pages/FacebookPage.js';
-import TwitterPage from '../components/pages/TwitterPage.js';
+import TwitterPage from '../components/pages/TwitterPage.js';*/
 import LoginPage from '../components/pages/LoginPage.js';
 
 let App = React.createClass({  
   render() {
     return (
       <div className="nav">
-        <Link to="app">Home</Link>
+       {/*<Link to="app">Home</Link>
         <Link to="fb">Facebook</Link>
-        <Link to="tweet">Twitter</Link>
+        <Link to="tweet">Twitter</Link>*/}
         <Link to="login">Login</Link>
 
         {/* this is the importTant part */}
@@ -25,9 +25,9 @@ let App = React.createClass({
 
 let routes = (  
   <Route name="app" path="/" handler={App}>
-    <Route name="home" path="/Home" handler={HomePage}/>
+    {/*<Route name="home" path="/Home" handler={HomePage}/>
     <Route name="fb" path="/Facebook" handler={FacebookPage}/>
-    <Route name="tweet" path="/Twitter" handler={TwitterPage}/>
+    <Route name="tweet" path="/Twitter" handler={TwitterPage}/>*/}
     <Route name="login" path="/login" handler={LoginPage}/>
   </Route>
 );
