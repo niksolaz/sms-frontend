6d88338d1beaa03c464a335511af6d702475669f
Nik Solaz <solazzo.nicola@gmail.com>
2015-11-20 16:36:58 +0000

    restored links in navigation

--- src/components/modules/shared/Navigation.jsx
+++ src/components/modules/shared/Navigation.jsx
@@ -2,21 +2,21 @@ import React from 'react';
 import Router from 'react-router';  
 import { DefaultRoute, Link, Route, RouteHandler } from 'react-router';
 
-{/*import HomePage from '../components/pages/HomePage.jsx';
-import FacebookPage from '../components/pages/FacebookPage.jsx';
-import TwitterPage from '../components/pages/TwitterPage.jsx';*/
+import HomePage from '../pages/HomePage.jsx';
+import FacebookPage from '../pages/FacebookPage.jsx';
+import TwitterPage from '../pages/TwitterPage.jsx';
 import LoginPage from '../pages/LoginPage.jsx';
 
 let Navigation = React.createClass({  
   render() {
     return (
       <div className="nav">
-        <Link to="app">Home</Link>
-        {/*<Link to="fb">Facebook</Link>
-        <Link to="tweet">Twitter</Link>*/}
+        <Link to="app">Nav</Link>
+        <Link to="home">Home</Link>
+        <Link to="fb">Facebook</Link>
+        <Link to="tweet">Twitter</Link>
         <Link to="login">Login</Link>
 
-        {/* this is the importTant part */}
         <RouteHandler/>
       </div>
     );
@@ -25,9 +25,9 @@ let Navigation = React.createClass({
 
 let routes = (  
   <Route name="app" path="/" handler={Navigation}>
-    {/*<Route name="home" path="/Home" handler={HomePage}/>
+    <Route name="home" path="/Home" handler={HomePage}/>
     <Route name="fb" path="/Facebook" handler={FacebookPage}/>
-    <Route name="tweet" path="/Twitter" handler={TwitterPage}/>*/}
+    <Route name="tweet" path="/Twitter" handler={TwitterPage}/>
     <Route name="login" path="/login" handler={LoginPage}/>
   </Route>
 );
