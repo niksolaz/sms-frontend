5c93ef9a7f57b4635a5929093a4810454e20cab5
Nik Solaz <solazzo.nicola@gmail.com>
2015-11-23 22:31:19 +0000

    import Header in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+{/*
+import Router from 'react-router';  
+import { DefaultRoute, Link, Route, RouteHandler } from 'react-router';
+
+import HeaderHandler from './components/modules/Header.jsx';
+*/}
 export default class App extends Component {
   render() {
     return (
       <div>
           This is your app!
+          <Headler />
       </div>
     );
   }
