a617085cf1edea98bad100b4f24e7f3501a8ea27
Nik Solaz <solazzo.nicola@gmail.com>
2015-11-26 10:46:47 +0000

    correct new import in navigation: imported pages

--- src/components/shared/Navigation.jsx
+++ src/components/shared/Navigation.jsx
@@ -1,13 +1,19 @@
 import React, { Component } from 'react'; 
 
-import LoginPage from '../pages/LoginPage.jsx'
+import HomePage from './HomePage.jsx';
+import FacebookPage from './FacebookPage.jsx';
+import TwitterPage from './TwitterPage.jsx';
+import LoginPage from './LoginPage.jsx';
 
 export default class Navigation extends Component {
   render() {
     return (
       <div>
           Navigation bar
+          <HomePage />
           <LoginPage />
+          <FacebookPage />
+          <TwitterPage />
       </div>
     );
   }
