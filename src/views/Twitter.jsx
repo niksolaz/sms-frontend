b65afa8cd74744c09a178a832de4b3d917c0ef83
Nik Solaz <solazzo.nicola@gmail.com>
2015-12-06 15:58:45 +0000

    Adding cookie checking in Twitter.jsx; More refactoring

--- src/views/Twitter.jsx
+++ src/views/Twitter.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import cookie from 'react-cookie';
 
 import Header from '../components/modules/Header.jsx';
 import {Link, PropTypes} from 'react-router';
@@ -7,13 +8,20 @@ export default class Twitter extends React.Component {
     constructor(props){
         super(props);
     }
-    /*
-    if user is logged in ->DO THIS: user see you message 
-    
-    if user is logged out ->DO THIS: user go to login page 
-    */
+    isLogged(){
+      /**
+       * It checks if the cookie of authentication is present
+       */
+      const cookieAuthentication = 'cookieMonster';
+      // If the cookie is present, return true otherwise false
+      return cookie.load(cookieAuthentication) !== null ? true: false;
+    }
     render() {
-        return (
+        /* Using the ternary operator <clause> ? <statement if clause is true> : <statement if the clause is false>*/
+        return !this.isLogged() ? 
+            // If it is not logged
+            <div className="container">Not logged</div> :
+            // If it is logged
             <div className="container">
                 <Header  />
                 <h3>Twitter</h3>
@@ -30,7 +38,6 @@ export default class Twitter extends React.Component {
                   </ul>
                 </div>
             </div>
-        );
     }
 }
 
