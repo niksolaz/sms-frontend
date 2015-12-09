4e0a85dcc9f328d6a5a37b5a41361de1db6faa85
Nik Solaz <solazzo.nicola@gmail.com>
2015-12-09 12:28:21 +0000

    create export class UserLoggedOut and UserLoggedIn

--- src/views/User.jsx
+++ src/views/User.jsx
@@ -9,23 +9,9 @@ const cookieAuthentication = 'cookieMonster';
 class UserLoggedIn extends Component{
     constructor(props){
         super(props);
-        
-        this.isLogged = this.isLogged.bind(this);
     }
     
     render(){
-        return
-    }
-}
-
-class UserLoggedOut extends Component{}
-
-export default class User extends React.Component {
-    constructor(props){
-        super(props);
-    }
-    
-    render() {
         let username = this.props.params.username;
         let message = username ? "I am " + username : "This is the user list";
         return (
@@ -39,6 +25,56 @@ export default class User extends React.Component {
                 </div>
             </div>
         );
+    }
+}
+
+class UserLoggedOut extends Component{
+    constructor(props){
+        super(props);
+    }
+    
+    render(){
+        return (
+            <div className="container">
+                <Link to={'/login'}>Login</Link>
+            </div>    
+        );
+    }
+}
+
+export default class User extends React.Component {
+    constructor(props){
+        super(props);
+        
+        this.isLogged = this.isLogged.bind(this);
+        this.onSuccessLogin = this.onSuccessLogin.bind(this);
+        this.onSuccessLogout = this.onSuccessLogout.bind(this);
+    }
+    
+    isLogged(){
+      // If the cookie is present, return true otherwise false
+      // If the user is log in, cookie.load(cookieAuthentication) will return the cookie string, 
+      // otherwise it will return undefined. 
+      const cookieValue = cookie.load(cookieAuthentication);
+     
+      const isUserLogged =  typeof( cookieValue ) !== 'undefined';
+      return isUserLogged;
+    }
+    
+    // When we login successfully from the system
+    onSuccessLogin(){
+        this.setState({logged: true});
+    }
+    
+    // When we logout successfully from the system
+    onSuccessLogout(){
+        this.setState({logged: false});
+    }
+    
+    render() {
+        return this.isLogged() ?
+            <UserLoggedOut userLogout={this.onSuccessLogout}/> :
+            <UserLoggedIn userLogin={this.onSuccessLogin}/>;
     }
 }
 
