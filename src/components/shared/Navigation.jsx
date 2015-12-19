b4d678bd6ef174d7687a620021af9a4290340d48
niksolaz <solazzo.nicola@gmail.com>
2015-12-19 12:05:39 +0100

    change component

--- src/components/shared/Navigation.jsx
+++ src/components/shared/Navigation.jsx
@@ -9,7 +9,7 @@ export default class Navigation extends Component {
       <nav className="navbar navbar-default">
           <div className="collapse navbar-collapse">
               <ul className="nav navbar-nav">
-                  <li className="navbar-link"><Link to={'/'}>Home</Link></li>
+                  <li className="navbar-link"><Link to={'/'}>Logo</Link></li>
                   <li className="navbar-link"><Link to={'/login'}>Login</Link></li>
                   <li className="navbar-link"><Link to={'/users'}>User</Link></li>
                   <li className="navbar-link"><Link to={'/Facebook'}>Facebook</Link></li>
