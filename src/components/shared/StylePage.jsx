5ed90830ef54fa204dd36d76c3cf83c6d7d6dcbc
niksolaz <solazzo.nicola@gmail.com>
2016-01-10 20:59:33 +0100

    ceate shared file pro style CSS  facebook and twitter

---
+++ src/components/shared/StylePage.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+export default class StylePage extends Component{
+  constructor(props){
+    super(props);
+  }
+  render(){
+    // To create CSS style 
+    };
+    return(
+        //return CSS style for page Facebook and Twitter
+    );
+  }
+}
