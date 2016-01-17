f8bc684a1e8a7dca464e2cf4320ed6eb65f72e23
niksolaz <solazzo.nicola@gmail.com>
2016-01-17 23:05:47 +0100

    new properties on switch button

--- src/components/shared/SettingButton.jsx
+++ src/components/shared/SettingButton.jsx
@@ -5,12 +5,17 @@ import Switch from 'react-bootstrap-switch';
 
 
 export default class SocialButton extends Component{
-
+  constructor(props){
+    super(props);
+  }
   render() {
     const styleSwitch = {
-      size : 'small' || null,
-      animate : true || false,
-      onColor : 'primary' || 'default',
+      size : 'small',
+      animate : true,
+      onColor : 'primary',
+      offColor: 'default',
+      onText: 'ON',
+      offText: 'OFF',
       baseClass : 'bootstrap-switch'   
     }
     return (
